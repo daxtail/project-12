3559eb0a1473c4da6831260b505b3d054134b1ea
daxtail <101337714+daxtail@users.noreply.github.com>
2022-03-25 13:52:42 +0530

    Add files via upload

---
+++ scratch_canvas (3).py
@@ -0,0 +1,17 @@
+n1 = int(input("Enter the first number"))
+n2 = int(input("Enter the second number"))
+
+def addition(n1,n2):
+    print(n1+n2)
+def subtraction(n1,n2):
+    print(n1-n2)
+def multiplication (n1,n2):
+    print(n1*n2)
+def division (n1,n2):
+    print(n1/n2)
+    
+addition(n1,n2)
+subtraction(n1,n2)
+multiplication (n1,n2)
+division (n1,n2)
+#Call both the functions and pass parameters n1, n2
